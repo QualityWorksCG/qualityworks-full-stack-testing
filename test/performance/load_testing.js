d1b7bcd36b70d324df2d05b195ab157bbf9f17aa
Javon Davis <javonldavis14@gmail.com>
2018-03-02 13:12:59 -0500

    Added examples for incorporation of load testing frameworks

--- test/performance/load_testing.js
+++ test/performance/load_testing.js
@@ -1,3 +1,26 @@
 /**
  * Created by javondavis on 3/2/18.
  */
+const run = require('autocannon/lib/run');
+
+function load_testing(url) {
+    var arguments =
+        {'url': url,
+         'connections': 10};
+    // autocannon.start(arguments)
+
+    const tracker = run(arguments);
+    tracker.on('done', function (result) {
+        if (arguments) {
+            console.log(JSON.stringify(result))
+        }
+    });
+
+    tracker.on('error', function (err) {
+        if (err) {
+            throw err
+        }
+    })
+}
+
+load_testing('http://localhost:3000');
